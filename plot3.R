e749036c235a35b3cdc1db117d6497854f8525eb
sergeypara <48568305+sergeypara@users.noreply.github.com>
2019-05-13 23:11:43 +0300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -32,3 +32,6 @@ with(t, {
 })
 legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
        c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+dev.copy(png, file="plot3.png", height=480, width=480) 
+dev.off()
